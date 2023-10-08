a24fab82b32d80d84dc13c98f9851c1c5862b99c
James Rehak <macbookair>
2023-10-08 12:59:31 -0400

    Add details to page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("I see you")
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry)=>{
         entry.target.classList.toggle('show', entry.isIntersecting);
@@ -7,3 +8,9 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+function getCurrentDateAndTime() {
+    const dateTime = new Date();
+    return dateTime.toLocaleString('en-us', { year:"numeric", month:"long", day:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
+}
+const currentDate = document.getElementById('currentDate');
+currentDate.innerHTML = getCurrentDateAndTime();
