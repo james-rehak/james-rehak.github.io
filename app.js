c77dfce516e8dadc3b192ab015131eac36b66f90
James <59107373+james-rehak@users.noreply.github.com>
2025-12-26 06:55:21 -0500

    2 add new invita experience (#3)
    * #2 Add new work experience
    
    * #2 Add invita experience. Fix typos.
    
    ---------
    
    Co-authored-by: rehak <rehak@rmba.local>
    Co-authored-by: James <jmba3>

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("I see you")
+console.warn("I see you");
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry)=>{
         entry.target.classList.toggle('show', entry.isIntersecting);
@@ -12,5 +12,8 @@ function getCurrentDateAndTime() {
     const dateTime = new Date();
     return dateTime.toLocaleString('en-us', { year:"numeric", month:"long", day:"numeric", hour:"numeric", minute:"numeric", second:"numeric"});
 }
-const currentDate = document.getElementById('currentDate');
-currentDate.innerHTML = getCurrentDateAndTime();
+const currentDates = document.getElementsByClassName('currentDate');
+
+for (let i = 0; i < currentDates.length; i++) {
+    currentDates[i].innerHTML = getCurrentDateAndTime();
+}
